2d52517e4b3a56cffbbd6ae2420ca61c3024327e
manske90 <manske90@gmail.com>
2021-04-21 19:45:04 -0700

    Delete setimages.java

--- setimages.java
+++
@@ -1,7 +0,0 @@
-// Runs the setImgs method of the Pokedex. No need for it in the GUI/main program as this is just a utility.
-public class setimages {
-    public static void main (String[] args) throws Exception {
-        Pokedex thisDex = new Pokedex();
-        thisDex.setImgs();
-    }
-}
